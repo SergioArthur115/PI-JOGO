f2736f0c720fe06c104de196cf3102c0647821ea
Sérgio Arthur Fialho M. Teixeira <182120042@senacinformatica.br>
2023-09-22 11:39:36 -0300

    comeco de implementação de carregamento do banco

--- php/carregar_informacao.php
+++ php/carregar_informacao.php
@@ -12,131 +12,59 @@ if ($conn->connect_error) {
     die("Falha na conexão com o banco de dados: " . $conn->connect_error);
 }
 
-$moeda = $_POST['moeda'];
-$mps = $_POST['mps'];
-$clique = $_POST['clique'];
-$entidadesQTD = $_POST['entidadesQTD'];
-$entidadesValor = $_POST['entidadesValor'];
-$artefatosValor = $_POST['artefatosValor'];
+$sql = "SELECT * FROM entidadesqtd WHERE id_entidadesQTD = (SELECT MAX(id_entidadesQTD) FROM entidadesqtd)";
 
-$entidadesQTDConvertido = json_decode($entidadesQTD);
-$entidadesValorConvertido = json_decode($entidadesValor);
-$artefatosValorConvertido = json_decode($artefatosValor);
+$sql1 = "SELECT * FROM entidadesvalor WHERE id_entidadesValor = (SELECT MAX(id_entidadesValor) FROM entidadesvalor)";
 
-$entidade1 = $entidadesQTDConvertido[0];
-$entidade2 = $entidadesQTDConvertido[1];
-$entidade3 = $entidadesQTDConvertido[2];
-$entidade4 = $entidadesQTDConvertido[3];
-$entidade5 = $entidadesQTDConvertido[4];
-$entidade6 = $entidadesQTDConvertido[5];
-$entidade7 = $entidadesQTDConvertido[6];
-$entidade8 = $entidadesQTDConvertido[7];
-$entidade9 = $entidadesQTDConvertido[8];
-$entidade10 = $entidadesQTDConvertido[9];
+$sql2 = "SELECT * FROM artefatosValor WHERE id_artefatosValor = (SELECT MAX(id_artefatosValor) FROM artefatosValor)";
 
-$entidadeValor1 = $entidadesValorConvertido[0];
-$entidadeValor2 = $entidadesValorConvertido[1];
-$entidadeValor3 = $entidadesValorConvertido[2];
-$entidadeValor4 = $entidadesValorConvertido[3];
-$entidadeValor5 = $entidadesValorConvertido[4];
-$entidadeValor6 = $entidadesValorConvertido[5];
-$entidadeValor7 = $entidadesValorConvertido[6];
-$entidadeValor8 = $entidadesValorConvertido[7];
-$entidadeValor9 = $entidadesValorConvertido[8];
-$entidadeValor10 = $entidadesValorConvertido[9];
-
-$artefato1 = $artefatosValorConvertido[0];
-$artefato2 = $artefatosValorConvertido[1];
-$artefato3 = $artefatosValorConvertido[2];
-$artefato4 = $artefatosValorConvertido[3];
-$artefato5 = $artefatosValorConvertido[4];
-$artefato6 = $artefatosValorConvertido[5];
-$artefato7 = $artefatosValorConvertido[6];
-$artefato8 = $artefatosValorConvertido[7];
-$artefato9 = $artefatosValorConvertido[8];
-$artefato10 = $artefatosValorConvertido[9];
-
-if ($clique == 1) {
-    $estado = true;
-    // Insere os dados no banco de dados
-    $sql = "INSERT INTO entidadesqtd (qtd1, qtd2, qtd3, qtd4, qtd5, qtd6, qtd7, qtd8, qtd9, qtd10) VALUES
-    ('$entidade1', '$entidade2', '$entidade3', '$entidade4', '$entidade5', '$entidade6', '$entidade7', '$entidade8', '$entidade9','$entidade10')";
-
-    $sql1 = "INSERT INTO entidadesvalor (valor1, valor2, valor3, valor4, valor5, valor6, valor7, valor8, valor9, valor10) 
-    VALUES ('$entidadeValor1', '$entidadeValor2', '$entidadeValor3', '$entidadeValor4', '$entidadeValor5', '$entidadeValor6', '$entidadeValor7', '$entidadeValor8', '$entidadeValor9','$entidadeValor10')";
-
-    $sql2 = "INSERT INTO artefatosValor (valor1, valor2, valor3, valor4, valor5, valor6, valor7, valor8, valor9, valor10) 
-    VALUES ('$artefato1', '$artefato2', '$artefato3', '$artefato4', '$artefato5', '$artefato6', '$artefato7', '$artefato8', '$artefato9','$artefato10')";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "entidadesQTD salva com sucesso!\n>";
-    } else {
-        echo "Erro ao salvar a informação: \n" . $conn->error;
-    }
-
-    if ($conn->query($sql1) === TRUE) {
-        echo "entidadesValor salva com sucesso!\n";
-    } else {
-        echo "Erro ao salvar a informação: \n" . $conn->error;
-    }
-
-    if ($conn->query($sql2) === TRUE) {
-        echo "artefatosValor salva com sucesso!\n";
-    } else {
-        echo "Erro ao salvar a informação: \n" . $conn->error;
-    }
-    $sql3 = "INSERT INTO cliente (moeda, mps, clique, id_entidadesQTD, id_entidadesValor, id_artefatosValor) VALUES ('$moeda', '$mps', '$clique', (SELECT MAX(id_entidadesQTD) FROM entidadesqtd), (SELECT MAX(id_entidadesValor) FROM entidadesvalor), (SELECT MAX(id_artefatosValor) FROM artefatosvalor))";
-
-    if ($conn->query($sql3) === TRUE) {
-        echo "cliente salva com sucesso!\n";
-    } else {
-        echo "Erro ao salvar a informação: \n" . $conn->error;
-    }
-    $conn->close();
+if ($conn->query($sql) === TRUE) {
+    echo "entidadesQTD carregada com sucesso!\n>";
+} else {
+    echo "Erro ao carregar a informação: \n" . $conn->error;
 }
 
-if ($clique != 1) {
-    echo "teste";
-    // Insere os dados no banco de dados
-    $sql = "UPDATE entidadesqtd SET qtd1 = '$entidade1', qtd2 = '$entidade2', qtd3 = '$entidade3', qtd4 = '$entidade4', qtd5 = '$entidade5', qtd6 = '$entidade6', qtd7 = '$entidade7', qtd8 = '$entidade8', qtd9 = '$entidade9', qtd10 = '$entidade10' WHERE id_entidadesqtd = (SELECT MAX(id_entidadesqtd) FROM entidadesqtd)";
-
-
-    $sql1 = "UPDATE entidadesvalor SET valor1 = '$entidadeValor1', valor2 = '$entidadeValor2', valor3 = '$entidadeValor3', valor4 = '$entidadeValor4', valor5 = '$entidadeValor5', valor6 = '$entidadeValor6', valor7 = '$entidadeValor7', valor8 = '$entidadeValor8', valor9 = '$entidadeValor9', valor10 = '$entidadeValor10' WHERE id_entidadesvalor = (SELECT MAX(id_entidadesvalor) FROM entidadesvalor)";
-
-    $sql2 = "UPDATE artefatosvalor SET valor1 = '$artefato1', valor2 = '$artefato2', valor3 = '$artefato3', valor4 = '$artefato4', valor5 = '$artefato5', valor6 = '$artefato6', valor7 = '$artefato7', valor8 = '$artefato8', valor9 = '$artefato9', valor10 = '$artefato10' WHERE id_artefatosvalor = (SELECT MAX(id_artefatosvalor) FROM artefatosvalor)";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "entidadesQTD atualizada com sucesso!\n";
-    } else {
-        echo "Erro ao atualizar a informação: \n" . $conn->error;
-    }
-
-    if ($conn->query($sql1) === TRUE) {
-        echo "entidadesValor atualizada com sucesso!\n";
-    } else {
-        echo "Erro ao atualizar a informação: \n" . $conn->error;
-    }
-
-    if ($conn->query($sql2) === TRUE) {
-        echo "artefatosValor atualizada com sucesso!\n";
-    } else {
-        echo "Erro ao atualizar a informação: \n" . $conn->error;
-    }
-
-    $sql3 = "UPDATE cliente 
-    SET moeda = '$moeda', 
-    mps = '$mps', 
-    clique = '$clique'  
-    WHERE id_entidadesqtd = (SELECT MAX(id_entidadesqtd)) 
-    AND id_entidadesvalor = (SELECT MAX(id_entidadesvalor)) 
-    AND id_artefatosvalor = (SELECT MAX(id_artefatosvalor)) 
-    AND id_cliente = (SELECT MAX(id_cliente))";
-
-    if ($conn->query($sql3) === TRUE) {
-        echo "cliente atualizada com sucesso!\n";
-    } else {
-        echo "Erro ao atualizar a informação: \n" . $conn->error;
-    }
-    $conn->close();
+if ($conn->query($sql1) === TRUE) {
+    echo "entidadesValor carregada com sucesso!\n";
+} else {
+    echo "Erro ao carregar a informação: \n" . $conn->error;
 }
+
+if ($conn->query($sql2) === TRUE) {
+    echo "artefatosValor carregada com sucesso!\n";
+} else {
+    echo "Erro ao carregar a informação: \n" . $conn->error;
+}
+$sql3 = "SELECT moeda, mps, clique FROM cliente WHERE id_entidadesQTD = (SELECT MAX(id_entidadesQTD) FROM entidadesqtd) AND id_entidadesValor = (SELECT MAX(id_entidadesValor) FROM entidadesvalor) AND id_artefatosValor = (SELECT MAX(id_artefatosValor) FROM artefatosvalor)";
+
+if ($conn->query($sql3) === TRUE) {
+    echo "cliente carregada com sucesso!\n";
+} else {
+    echo "Erro ao carregar a informação: \n" . $conn->error;
+}
+$conn->close();
+
+
+for ($i=1; $i <= 10; $i++) { 
+    $entidadesQTD[$i-1] = $sql[$i];
+}
+for ($i=1; $i <= 10; $i++) { 
+    $entidadesValor[$i-1] = $sql1[$i];
+}
+for ($i=1; $i <= 10; $i++) { 
+    $artefatosValor[$i-1] = $sql2[$i];
+}
+$moeda = $sql2[0];
+$mps = $sql2[1];
+$clique = $sql2[2];
+
+// Passando as informações para o JavaScript
+echo "<script>";
+echo "var entidadesQTD = " . json_encode($entidadesQTD) . ";";
+echo "var entidadesValor = " . json_encode($entidadesValor) . ";";
+echo "var artefatosValor = " . json_encode($artefatosValor) . ";";
+echo "var moeda = " . $cliente['moeda'] . ";";
+echo "var mps = " . $cliente['mps'] . ";";
+echo "var clique = " . $cliente['clique'] . ";";
+echo "</script>";
 ?>
--- php/cosmicclicker.sql
+++ php/cosmicclicker.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 15-Set-2023 às 21:37
+-- Tempo de geração: 22-Set-2023 às 15:44
 -- Versão do servidor: 10.4.27-MariaDB
--- versão do PHP: 8.1.6
+-- versão do PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -41,6 +41,17 @@ CREATE TABLE `artefatosvalor` (
   `valor10` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Extraindo dados da tabela `artefatosvalor`
+--
+
+INSERT INTO `artefatosvalor` (`id_artefatosValor`, `valor1`, `valor2`, `valor3`, `valor4`, `valor5`, `valor6`, `valor7`, `valor8`, `valor9`, `valor10`) VALUES
+(39, 'Comprado', 'Comprado', 'Comprado', '3038', '35129', '547435', '2059060', '14239150', '976139168', '8681402912'),
+(40, 'Comprado', '100', '449', '3038', '35129', '547435', '2059060', '14239150', '976139168', '8681402912'),
+(41, 'Comprado', '100', '449', '3038', '35129', '547435', '2059060', '14239150', '976139168', '8681402912'),
+(42, 'Comprado', '100', '449', '3038', '35129', '547435', '2059060', '14239150', '976139168', '8681402912'),
+(43, 'Comprado', 'Comprado', '449', '3038', '35129', '547435', '2059060', '14239150', '976139168', '8681402912');
+
 -- --------------------------------------------------------
 
 --
@@ -51,11 +62,22 @@ CREATE TABLE `cliente` (
   `id_cliente` int(11) NOT NULL,
   `moeda` float NOT NULL,
   `mps` float NOT NULL,
-  `clique` bigint(20) NOT NULL,
+  `clique` bigint(100) NOT NULL,
   `id_entidadesQTD` int(11) DEFAULT NULL,
   `id_entidadesValor` int(11) DEFAULT NULL,
   `id_artefatosValor` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Extraindo dados da tabela `cliente`
+--
+
+INSERT INTO `cliente` (`id_cliente`, `moeda`, `mps`, `clique`, `id_entidadesQTD`, `id_entidadesValor`, `id_artefatosValor`) VALUES
+(39, 378.55, 1.6, 12, 40, 39, 39),
+(40, 378.55, 1.6, 12, 41, 40, 40),
+(41, 378.55, 1.6, 12, 42, 41, 41),
+(42, 378.55, 1.6, 12, 43, 42, 42),
+(43, 378.55, 1.6, 12, 44, 43, 43);
 
 -- --------------------------------------------------------
 
@@ -76,6 +98,17 @@ CREATE TABLE `entidadesqtd` (
   `qtd9` int(11) NOT NULL,
   `qtd10` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `entidadesqtd`
+--
+
+INSERT INTO `entidadesqtd` (`id_entidadesQTD`, `qtd1`, `qtd2`, `qtd3`, `qtd4`, `qtd5`, `qtd6`, `qtd7`, `qtd8`, `qtd9`, `qtd10`) VALUES
+(40, 14, 11, 1, 0, 0, 0, 0, 0, 0, 0),
+(41, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(42, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(43, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(44, 6, 1, 0, 0, 0, 0, 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -98,6 +131,17 @@ CREATE TABLE `entidadesvalor` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Extraindo dados da tabela `entidadesvalor`
+--
+
+INSERT INTO `entidadesvalor` (`id_entidadesValor`, `valor1`, `valor2`, `valor3`, `valor4`, `valor5`, `valor6`, `valor7`, `valor8`, `valor9`, `valor10`) VALUES
+(39, 37.96, 285.33, 4533.1, 11727, 848309, 1964830, 10363300, 569079000, 9641580000, 23493900000),
+(40, 14.64, 100, 4121, 11727, 848309, 1964830, 10363300, 569079000, 9641580000, 23493900000),
+(41, 10, 100, 4121, 11727, 848309, 1964830, 10363300, 569079000, 9641580000, 23493900000),
+(42, 10, 100, 4121, 11727, 848309, 1964830, 10363300, 569079000, 9641580000, 23493900000),
+(43, 17.71, 110, 4121, 11727, 848309, 1964830, 10363300, 569079000, 9641580000, 23493900000);
+
+--
 -- Índices para tabelas despejadas
 --
 
@@ -112,9 +156,9 @@ ALTER TABLE `artefatosvalor`
 --
 ALTER TABLE `cliente`
   ADD PRIMARY KEY (`id_cliente`),
-  ADD KEY `id_entidadesQTD` (`id_entidadesQTD`),
-  ADD KEY `id_entidadesValor` (`id_entidadesValor`),
-  ADD KEY `id_artefatosValor` (`id_artefatosValor`);
+  ADD KEY `cliente_ibfk_1` (`id_entidadesQTD`),
+  ADD KEY `cliente_ibfk_2` (`id_entidadesValor`),
+  ADD KEY `cliente_ibfk_3` (`id_artefatosValor`);
 
 --
 -- Índices para tabela `entidadesqtd`
@@ -136,25 +180,25 @@ ALTER TABLE `entidadesvalor`
 -- AUTO_INCREMENT de tabela `artefatosvalor`
 --
 ALTER TABLE `artefatosvalor`
-  MODIFY `id_artefatosValor` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_artefatosValor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT de tabela `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT de tabela `entidadesqtd`
 --
 ALTER TABLE `entidadesqtd`
-  MODIFY `id_entidadesQTD` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_entidadesQTD` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 
 --
 -- AUTO_INCREMENT de tabela `entidadesvalor`
 --
 ALTER TABLE `entidadesvalor`
-  MODIFY `id_entidadesValor` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_entidadesValor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- Restrições para despejos de tabelas
